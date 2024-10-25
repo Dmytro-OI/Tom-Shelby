da269f83d9bd4b680f38da6a526d642d1d317bd6
dmytr <dmytro.hrynchyshyn.oi.2023@lpnu.ua>
2024-10-25 23:55:27 +0300

    YA DOWN

--- transcription.py
+++ transcription.py
@@ -1,22 +1,30 @@
 from youtube_transcript_api import YouTubeTranscriptApi
+from pytube import extract
 
 class Transcriptor:
-    def __init__(self,video_id):
-        self.video_id = video_id
+    def __init__(self, url):
+        self.video_id = self.get_ID(url)
+
+    def get_ID(self, url):
+        video_id = extract.video_id(url)
+        return video_id
 
     def transcript_text(self):
-        transcript_list = YouTubeTranscriptApi.list_transcripts(video_id)
+        transcript_list = YouTubeTranscriptApi.list_transcripts(self.video_id)
 
-        all_filtered_text = []
+        full_text = ""
+        
         for transcript in transcript_list:
-            print(
-                transcript.is_generated,
-            )
-        transcript_data = transcript.fetch()
-
-        filtered_text = [
-                entry['text'] for entry in transcript_data 
-                if entry['text'].strip() not in ["[Music]"]
-            ]
-        all_filtered_text.extend(filtered_text)
-        return all_filtered_text
+            if transcript.is_generated:
+                transcript_data = transcript.fetch()
+                
+                for entry in transcript_data:
+                    full_text += entry['text'] + " "
+                
+        return full_text
+    
+if __name__ == "__main__":
+    url = "https://www.youtube.com/watch?v=7iwgyzX-76g"  
+    transcriptor = Transcriptor(url)
+    transcript_text = transcriptor.transcript_text()
+    print(transcript_text)
