597af3e610dced19629a62bdd3cef5ba981828d0
Sviatoslav <sviatkostehnii@gmail.com>
2024-10-26 16:41:19 +0300

    feat: add server to make summary

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 from transcription import Transcriptor
 from summariser import Summariser
 
+from fastapi import FastAPI
+from pydantic import BaseModel
 
 
-def main():
-    t = Transcriptor("https://www.youtube.com/watch?v=LaffA9EyUgo")
+class SummaryBody(BaseModel):
+    link: str
+    lang: str
+    max_length: float
+
+app = FastAPI()
+
+
+@app.post("/")
+async def get_summary(body: SummaryBody):
+
+    t = Transcriptor(body.link)
     text = t.transcript_text()
     s = Summariser()
     summary = s.get_summary(text, 100)
 
-    with open("summary.txt", mode="w") as f:
-        f.write(summary)
-
-
-
-if __name__ == "__main__":
-    main()
+    return {"status": 200, "data": {"summary": summary}}
