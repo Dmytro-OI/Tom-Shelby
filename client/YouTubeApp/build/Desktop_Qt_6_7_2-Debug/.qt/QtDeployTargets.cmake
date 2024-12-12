45cb27725b68487126a7ea075c4ee3224d15ac43
Sviatoslav <sviatkostehnii@gmail.com>
2024-12-12 21:51:43 +0200

    minor changes

--- client/YouTubeApp/build/Desktop_Qt_6_7_2-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_YouTubeApp_FILE /home/sviatkohuet/Documents/routine/dmytro/YouTubeApp/build/Desktop_Qt_6_7_2-Debug/YouTubeApp)
-set(__QT_DEPLOY_TARGET_YouTubeApp_TYPE EXECUTABLE)
--- client/YouTubeApp/build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_YouTubeApp_FILE C:/Users/dmytr/YouTubeApp/build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/YouTubeApp.exe)
-set(__QT_DEPLOY_TARGET_YouTubeApp_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_YouTubeApp_RUNTIME_DLLS F:/Qt/6.8.0/mingw_64/bin/Qt6Widgets.dll;F:/Qt/6.8.0/mingw_64/bin/Qt6Gui.dll;F:/Qt/6.8.0/mingw_64/bin/Qt6Core.dll)
